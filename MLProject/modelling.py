9e36d26dea733683dd4e16b76f01967c59f54f7a
rifkyadiii <rifkyadikusumah@gmail.com>
2025-07-23 22:57:11 +0700

    MUDAH MUDAHAN FIX

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import mlflow
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+
+# Set tracking URI ke parent directory
+mlflow.set_tracking_uri("file:../mlruns")
 
 # Muat Data
 df = pd.read_csv('dataset_preprocessing/Telco-Customer-Churn_preprocessing.csv')
