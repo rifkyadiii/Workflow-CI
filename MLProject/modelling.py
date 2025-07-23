6d08772c6855ec1ef409aebcc900d2699848c2fa
rifkyadiii <rifkyadikusumah@gmail.com>
2025-07-23 22:32:10 +0700

    TES

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Mulai Run
 with mlflow.start_run():
     # Muat Data
     df = pd.read_csv('dataset_preprocessing/Telco-Customer-Churn_preprocessing.csv')
