b3c82145626fb4760ca9e240ae7878691bb3eef8
rifkyadiii <rifkyadikusumah@gmail.com>
2025-07-23 21:44:20 +0700

    first commit

---
+++ MLProject/modelling.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import mlflow
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Mulai Run
+with mlflow.start_run():
+    # Muat Data
+    df = pd.read_csv('dataset_preprocessing/Telco-Customer-Churn_preprocessing.csv')
+    X = df.drop('Churn', axis=1)
+    y = df['Churn']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Latih model dengan parameter terbaik
+    n_estimators = 100
+    max_depth = 20
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("max_depth", max_depth)
+    
+    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
+    model.fit(X_train, y_train)
+
+    # Evaluasi dan Log
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    mlflow.log_metric("accuracy", accuracy)
+    
+    # Log model
+    mlflow.sklearn.log_model(model, "model")
